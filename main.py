8742dbe93ce53e6b47fd8c2f7326e05d5419a57b
jaswanth kumar m <jashukumar.mallela@gmail.com>
2026-08-09 15:54:09 +0530

    checking telegram message functionality

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ def split_message(text, max_len=2000):
     return [text[i:i+max_len] for i in range(0, len(text), max_len)]
 
 
+
 def send_telegram_message(text):
     token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
-
-    # print("token:", token)
-    # print("chat_id:", chat_id)
 
     url = f"https://api.telegram.org/bot{token}/sendMessage"
 
@@ -23,18 +21,21 @@ def send_telegram_message(text):
             "chat_id": chat_id,
             "text": msg
         }
+
         response = requests.post(url, json=payload)
 
-    print("status:", response.status_code)
-    print("response:", response.text)
+        print("status:", response.status_code)
+        print("response:", response.text)
+
+
 
 
 def main():
     articles = get_news()
-    report = summarize(articles)
+    # report = summarize(articles)
 
-    print(report)  # still useful for GitHub logs
-    send_telegram_message(report)
+    # print(report)  # still useful for GitHub logs
+    send_telegram_message("Hi")
 
 
 if __name__ == "__main__":
